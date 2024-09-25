27afc94353f0423bb46a68f69babec7f550ffb1c
Max Melendez <max.melendez@predictum.com>
2024-09-25 00:55:53 -0400

    Displayed list

--- vehicle-list-ui/src/app/page.tsx
+++ vehicle-list-ui/src/app/page.tsx
@@ -1,101 +1,83 @@
-import Image from "next/image";
+'use client'
+import useSWR from 'swr'
+import axios from 'axios';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="">
+      <main className="">
+        <VehicleList/>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
+interface ManufacturerProps {
+  Id?: number;
+  ShortName?: string;
+  FullName?: string;
+  Country?: string;
+}
+
+interface ManufacturerListProps {
+  manufacturers?: ManufacturerProps[];
+}
+
+function VehicleList (){
+  const { data, error, isLoading } = useSWR("http://localhost:5106/vehicles", fetcher);
+  return (
+    <div className="mt-5 flex justify-between flex-row">
+      {!error && !isLoading && data && data.length > 0 && data.map((vehicleType, index) => {
+            console.log(vehicleType);
+            const type = String(vehicleType.VehicleType);
+            return(
+              <div key={index} className="bg-gray-500 shadow-md rounded-lg min-w-[250px] max-w-[250px] overflow-y-scroll max-h-[1000px] p-1">
+                <div className="flex flex-col items-center text-l font-semibold text-white min-h-[50px] mb-2">
+                  {type}
+                </div>
+                <ManufacturerList manufacturers={vehicleType.Manufacturers}/>
+              </div>
+            )
+          }
+        )}
+    </div>
+  );
+}
+
+function ManufacturerList ({manufacturers}:ManufacturerListProps){
+  return (
+    <div>
+      {manufacturers && manufacturers.length > 0 && manufacturers.map((vehicle, index) => {
+            const id = vehicle.Id;
+            const shortName = vehicle.ShortName;
+            const fullName = vehicle.FullName;
+            const country = vehicle.Country;
+            console.log(vehicle)
+            return(
+              <div key={index} className="flex flex-col bg-white shadow-md rounded-lg min-w-[200px] mb-5 p-5">
+                <div className="text-sm font-semibold text-black mb-2">
+                  {id}
+                </div>
+                <div className="text-sm font-semibold text-black mb-2">
+                  {shortName}
+                </div>
+                <div className="text-sm font-semibold text-black mb-2">
+                  {fullName}
+                </div>
+                <div className="text-sm font-semibold text-black">
+                  {country}
+                </div>
+              </div>
+            )
+          }
+        )}
+    </div>
+  );
+}
+
+
+const fetcher = async (url) => {
+  const { data } = await axios.get(url);
+  return data;
+};
